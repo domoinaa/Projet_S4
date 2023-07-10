16160146a49666c8962765c637eb804cb47152b0
IhaingotianaRamilijaona <ihaingoram@gmail.com>
2023-07-10 21:42:33 +0300

    Views codeigniter

---
+++ application/views/inscription.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="Projet_S4/Welcome/get_inscription" method="post">
+        <input type="text" name="Nom" >
+        <input type="number" name="Age" >
+        <input type="number" name="Poids" >
+        <select name="sexe">
+            <option value="femme">femme</option>
+            <option value="homme">homme</option>
+        </select>
+        <input type="number" name="taille" >
+        <input type="text" name="mdp" >
+        <input type="submit" value="S'inscrire">
+    </form>
+</body>
+</html>
---
+++ application/views/login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="Projet_S4/Welcome/makaLogin" method="post">
+        <input type="text" name="nom">
+        <input type="text" name="mdp">
+        <input type="submit" value="Se connecter">
+    </form>
+</body>
+</html>
