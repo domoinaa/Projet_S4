2a257f909544dc34c577797774d8c3d55da1b9e5
IhaingotianaRamilijaona <ihaingoram@gmail.com>
2023-07-11 15:48:02 +0300

    integration css login et inscription

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'C:\xampp\htdocs\REGIME\Projet_S4';
+$config['base_url'] = 'http://localhost/Regime/Projet_S4/';
 
 /*
 |--------------------------------------------------------------------------
